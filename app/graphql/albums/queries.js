5cfb271fe86bd343facbfe615e07d8f385031880
Alejo Acevedo <alejo.acevedo@wolox.com.ar>
2019-08-05 14:49:44 -0300

    Add Get All Albums

--- app/graphql/albums/queries.js
+++ app/graphql/albums/queries.js
@@ -3,11 +3,13 @@ const albumsService = require('../../services/album');
 
 module.exports = {
   queries: {
-    album: (_, params) => albumsService.getAlbum(params.id)
+    album: (_, params) => albumsService.getAlbum(params.id),
+    albums: (_, params) => albumsService.getAllAlbums(params.offset, params.limit, params.orderBy)
   },
   schema: gql`
     extend type Query {
       album(id: ID): Album!
+      albums(offset: Int, limit: Int, orderBy: String): [Album!]!
     }
   `
 };
--- app/services/album.js
+++ app/services/album.js
@@ -32,3 +32,32 @@ exports.getArtistOfAlbum = async userId => {
     throw errors.conectionError(e.message);
   }
 };
+
+exports.getAllAlbums = async (offset, limit, orderBy = 'id') => {
+  const endpoint = `${url}albums`;
+  try {
+    let albums = await fetch(endpoint);
+    albums = await albums.json();
+    albums.sort((a, b) => {
+      if (!a[orderBy] || !b[orderBy]) {
+        throw errors.badRequest('The orderBy parameter do not exist');
+      }
+      if (a[orderBy] < b[orderBy]) {
+        return -1;
+      }
+      if (a[orderBy] > b[orderBy]) {
+        return 1;
+      }
+      return 0;
+    });
+    if (offset !== undefined && limit !== undefined) {
+      albums = albums.slice(offset, offset + limit);
+    }
+    return albums;
+  } catch (e) {
+    if (e.extensions) {
+      throw e;
+    }
+    throw errors.conectionError(e.message);
+  }
+};
