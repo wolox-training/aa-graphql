b0d9ad815eca78619ca1f74fe02359a023ea9135
aleacevedo <ale.acevedo@live.com.ar>
2019-08-06 14:01:42 -0300

    Change node-fetch for axios. And await for then

--- app/services/album.js
+++ app/services/album.js
@@ -23,31 +23,30 @@ exports.getPhotosOfAlbum = albumId => {
     });
 };
 
-exports.getAllAlbums = async (offset, limit, orderBy = 'id') => {
+exports.getAllAlbums = (offset, limit, orderBy = 'id') => {
   const endpoint = `${url}albums`;
-  try {
-    let albums = await fetch(endpoint);
-    albums = await albums.json();
-    albums.sort((a, b) => {
-      if (!a[orderBy] || !b[orderBy]) {
-        throw errors.badRequest('The orderBy parameter do not exist');
+  return axios
+    .get(endpoint)
+    .then(response => response.data)
+    .catch(e => {
+      throw errors.conectionError(e.message);
+    })
+    .then(albums => {
+      albums.sort((a, b) => {
+        if (!a[orderBy] || !b[orderBy]) {
+          throw errors.badRequest('The orderBy parameter do not exist');
+        }
+        if (a[orderBy] < b[orderBy]) {
+          return -1;
+        }
+        if (a[orderBy] > b[orderBy]) {
+          return 1;
+        }
+        return 0;
+      });
+      if (offset !== undefined && limit !== undefined) {
+        return albums.slice(offset, offset + limit);
       }
-      if (a[orderBy] < b[orderBy]) {
-        return -1;
-      }
-      if (a[orderBy] > b[orderBy]) {
-        return 1;
-      }
-      return 0;
+      return albums;
     });
-    if (offset !== undefined && limit !== undefined) {
-      albums = albums.slice(offset, offset + limit);
-    }
-    return albums;
-  } catch (e) {
-    if (e.extensions) {
-      throw e;
-    }
-    throw errors.conectionError(e.message);
-  }
 };
