15a7ef8db3a224c11ad9f2cfdcb0395fa2d381d2
aleacevedo <ale.acevedo@live.com.ar>
2019-08-06 15:42:08 -0300

    Add password encryption

--- app/services/user.js
+++ app/services/user.js
@@ -1,9 +1,33 @@
 const errors = require('../errors'),
-  { user: User } = require('../models');
+  { user: User } = require('../models'),
+  bcrypt = require('bcrypt');
+
+const logger = require('../logger');
+
+const saltRounds = 10;
 
 exports.createUser = user =>
-  User.createModel(user)
-    .then(userCreated => userCreated)
+  bcrypt
+    .hash(user.password, saltRounds)
+    .then(hash =>
+      User.createModel({
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        password: hash
+      })
+        .then(newUser => {
+          logger.info('User created');
+          return newUser;
+        })
+        .catch(e => {
+          throw errors.databaseError(e.message);
+        })
+    )
     .catch(e => {
+      logger.error(e.message);
+      if (e.extensions) {
+        throw e;
+      }
       throw errors.defaultError(e.message);
     });
