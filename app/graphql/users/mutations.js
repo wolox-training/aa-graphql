9780eb954ac400dfeb9b427b6372bc3796b77157
aleacevedo <ale.acevedo@live.com.ar>
2019-08-07 14:12:41 -0300

    Remove kik-off examples

--- app/graphql/users/mutations.js
+++ app/graphql/users/mutations.js
@@ -1,27 +1,14 @@
 const { gql } = require('apollo-server'),
-  { userLoggedIn } = require('../events'),
   usersService = require('../../services/user');
 
 module.exports = {
   mutations: {
     user: (_, { firstName, lastName, email, password }) =>
-      usersService.createUser({ firstName, lastName, email, password }),
-    createUser: (_, { user }) => usersService.createUser(user),
-    login: (_, { credentials }) => {
-      // IMPORTANT: Not a functional login, its just for illustrative purposes
-      userLoggedIn.publish(credentials.username);
-      return {
-        accessToken: 'example_token',
-        refreshToken: 'example_refresh_token',
-        expiresIn: 134567899123
-      };
-    }
+      usersService.createUser({ firstName, lastName, email, password })
   },
   schema: gql`
     extend type Mutation {
       user(firstName: String!, lastName: String!, email: String!, password: String!): User!
-      createUser(user: UserInput!): User!
-      login(credentials: LoginInput!): AccessToken
     }
   `
 };
