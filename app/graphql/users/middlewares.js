47cf76348f50121ce3735b410677f1f91df58e77
aleacevedo <ale.acevedo@live.com.ar>
2019-08-09 14:37:06 -0300

    Add login middleware

--- app/graphql/users/middlewares.js
+++ app/graphql/users/middlewares.js
@@ -1,14 +1,19 @@
 const usersMiddleware = require('../../middlewares/user.js');
 
 const user = (resolve, root, args) => {
-  usersMiddleware.validateEmail(args.email);
-  usersMiddleware.validatePassword(args.password);
+  usersMiddleware.validateEmail(args.user.email);
+  usersMiddleware.validatePassword(args.user.password);
+  return resolve(root, args);
+};
+const logIn = (resolve, root, args) => {
+  usersMiddleware.validateEmail(args.logIn.email);
   return resolve(root, args);
 };
 
 module.exports = {
   // Here you add all the middlewares for the mutations, queries or field resolvers if you have any
   mutations: {
-    user
+    user,
+    logIn
   }
 };
