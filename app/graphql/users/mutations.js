3663017594730f7a4d30191d849e50f7ae6d946c
aleacevedo <ale.acevedo@live.com.ar>
2019-08-09 14:35:58 -0300

    Add login mutation

--- app/graphql/users/mutations.js
+++ app/graphql/users/mutations.js
@@ -3,11 +3,13 @@ const { gql } = require('apollo-server'),
 
 module.exports = {
   mutations: {
-    user: (_, { user }) => usersService.createUser(user)
+    user: (_, { user }) => usersService.createUser(user),
+    logIn: (_, { logIn }) => usersService.logIn(logIn)
   },
   schema: gql`
     extend type Mutation {
       user(user: UserInput!): User!
+      logIn(logIn: LoginInput!): String!
     }
   `
 };
