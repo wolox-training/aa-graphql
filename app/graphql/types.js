b667ae022fabe2697a1aa4e7113f69431e53e70b
aleacevedo <ale.acevedo@live.com.ar>
2019-08-12 13:54:26 -0300

    Add cache control

--- app/graphql/types.js
+++ app/graphql/types.js
@@ -1,10 +1,16 @@
 const { gql } = require('apollo-server');
 
 module.exports = gql`
+  enum CacheControlScope {
+    PUBLIC
+    PRIVATE
+  }
+
+  directive @cacheControl(maxAge: Int, scope: CacheControlScope) on FIELD_DEFINITION | OBJECT | INTERFACE
   type Query
   type Mutation
   type Subscription
-  type User {
+  type User @cacheControl(maxAge: 240) {
     firstName: String! @deprecated(reason: "We join in name")
     lastName: String! @deprecated(reason: "We join in name")
     name: String!
@@ -17,12 +23,12 @@ module.exports = gql`
     #refreshToken: String!
     #expiresIn: Int!
   }
-  type Album {
+  type Album @cacheControl(maxAge: 240) {
     id: ID!
     title: String!
     photos: [Photo!]!
   }
-  type Photo {
+  type Photo @cacheControl(maxAge: 240) {
     albumId: ID!
     id: Int!
     title: String!
