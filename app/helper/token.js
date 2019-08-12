54867bd1cab6028548205958c2d5769a3dad5157
aleacevedo <ale.acevedo@live.com.ar>
2019-08-12 10:48:09 -0300

    Add encryption and decode function

--- app/helper/token.js
+++ app/helper/token.js
@@ -1,5 +1,20 @@
 const jwt = require('jsonwebtoken');
 
+const errors = require('../errors');
 const secretKey = require('../../config').common.jwt.secret_key;
 
-exports.encrypt = data => jwt.sign(data, secretKey);
+exports.encrypt = data => {
+  try {
+    return jwt.sign(data, secretKey);
+  } catch (e) {
+    throw errors.defaultError(e.message);
+  }
+};
+
+exports.decode = token => {
+  try {
+    return jwt.verify(token, secretKey);
+  } catch (e) {
+    return null;
+  }
+};
