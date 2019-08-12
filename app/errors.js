e72fb424a900adeade7dd28037dac89cad2f85b8
aleacevedo <ale.acevedo@live.com.ar>
2019-08-12 10:46:25 -0300

    Add not found error

--- app/errors.js
+++ app/errors.js
@@ -5,6 +5,7 @@ const createError = (message, statusCode) => new ApolloError(message, statusCode
 const DEFAULT_ERROR = 500,
   BAD_REQUEST = 400,
   UNAUTHORIZED = 401,
+  NOT_FOUND = 404,
   CONNECTION_ERROR = 502,
   DATABASE_ERROR = 503;
 
@@ -13,3 +14,4 @@ exports.badRequest = message => createError(message, BAD_REQUEST);
 exports.conectionError = message => createError(message, CONNECTION_ERROR);
 exports.databaseError = message => createError(message, DATABASE_ERROR);
 exports.unauthorized = message => createError(message, UNAUTHORIZED);
+exports.notFound = message => createError(message, NOT_FOUND);
