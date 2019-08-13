28dc5cc42b22bc3f5ce39b702f9632fd4bb6bb53
aleacevedo <ale.acevedo@live.com.ar>
2019-08-13 16:39:48 -0300

    Add format error

--- test/server.spec.js
+++ test/server.spec.js
@@ -2,7 +2,15 @@ const { createTestClient } = require('apollo-server-testing'),
   { ApolloServer } = require('apollo-server'),
   schema = require('../app/graphql');
 
-const { query: _query, mutate } = createTestClient(new ApolloServer({ schema }));
+const { query: _query, mutate } = createTestClient(
+  new ApolloServer({
+    schema,
+    formatError: err => ({
+      message: err.message,
+      statusCode: err.extensions.code
+    })
+  })
+);
 
 const query = params => _query({ query: params });
 
