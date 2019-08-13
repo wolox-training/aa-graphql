75d58f916b0e9d0f15b84b11f3ceb934aeac89fe
aleacevedo <ale.acevedo@live.com.ar>
2019-08-13 12:16:30 -0300

    More expressive errors

--- app/helper/validator.js
+++ app/helper/validator.js
@@ -3,10 +3,10 @@ const { badRequest } = require('../errors');
 
 exports.validateEmail = email => {
   if (!isEmail(email)) {
-    throw badRequest('No valid email');
+    throw badRequest('No valid format email. It should be example@wolox.com.ar');
   }
   if (!email.includes('@wolox.com.ar')) {
-    throw badRequest('Not wolox email');
+    throw badRequest('Not wolox email. It should be example@wolox.com.ar');
   }
 };
 
@@ -15,6 +15,6 @@ exports.validatePassword = password => {
     throw badRequest('Not alphanumeric password');
   }
   if (!isLength(password, { min: 8 })) {
-    throw badRequest('Not long enough password');
+    throw badRequest('Not long enough password. Minimun length is 8');
   }
 };
