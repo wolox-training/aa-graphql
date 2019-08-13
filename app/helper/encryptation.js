0d7a2f1e3106375b12360380bbb883adb5f4d73c
aleacevedo <ale.acevedo@live.com.ar>
2019-08-13 10:50:40 -0300

    Use bcryptjs

--- app/helper/encryptation.js
+++ app/helper/encryptation.js
@@ -1,11 +1,13 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const errors = require('../errors');
 const logger = require('../logger');
 
 const saltRounds = 10;
 
 exports.encrypt = password =>
-  bcrypt.hash(password, saltRounds).catch(e => {
-    logger.error(e.message);
-    throw errors.defaultError(e);
-  });
+  bcrypt.genSalt(saltRounds).then(salt =>
+    bcrypt.hash(password, salt).catch(e => {
+      logger.error(e.message);
+      throw errors.defaultError(e);
+    })
+  );
