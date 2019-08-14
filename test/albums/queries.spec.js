a3924e3a7149890a520709ef4f56f3daaa633bee
aleacevedo <ale.acevedo@live.com.ar>
2019-08-14 11:24:33 -0300

    Convert to Promise sintax

--- test/albums/queries.spec.js
+++ test/albums/queries.spec.js
@@ -6,55 +6,67 @@ const { query } = require('../server.spec'),
 
 describe('albums', () => {
   describe('queries', () => {
-    it('should get album properly', async () => {
-      const fakeAlbums = await albumFactory.attributesMany(3);
-      const fakePhotos = await photoFactory.attributesMany(5);
-      await axios.setMockAlbums(fakeAlbums);
-      await axios.setMockPhotos(fakePhotos);
-      await query(getAlbum(fakeAlbums[0].id)).then(res =>
-        expect(res.data).toEqual({
-          album: {
-            userId: fakeAlbums[0].userId,
-            id: fakeAlbums[0].id,
-            title: fakeAlbums[0].title,
-            photos: fakePhotos
-          }
-        })
-      );
-      await query(getAlbum(fakeAlbums[1].id)).then(res =>
-        expect(res.data).toEqual({
-          album: {
-            userId: fakeAlbums[1].userId,
-            id: fakeAlbums[1].id,
-            title: fakeAlbums[1].title,
-            photos: fakePhotos
-          }
-        })
-      );
-      await query(getAlbum(fakeAlbums[2].id)).then(res =>
-        expect(res.data).toEqual({
-          album: {
-            userId: fakeAlbums[2].userId,
+    it('should get album properly', () => {
+      const fakeAlbumsProm = albumFactory.attributesMany(3).then(fakeAlbumsToMock => {
+        axios.setMockAlbums(fakeAlbumsToMock);
+        return fakeAlbumsToMock;
+      });
+      const fakePhotosProm = photoFactory.attributesMany(5).then(fakePhotosToMock => {
+        axios.setMockPhotos(fakePhotosToMock);
+        return fakePhotosToMock;
+      });
+      Promise.all([fakeAlbumsProm, fakePhotosProm]).then(([fakeAlbums, fakePhotos]) => {
+        query(getAlbum(fakeAlbums[0].id)).then(res =>
+          expect(res.data).toEqual({
+            album: {
+              userId: fakeAlbums[0].userId,
+              id: fakeAlbums[0].id,
+              title: fakeAlbums[0].title,
+              photos: fakePhotos
+            }
+          })
+        );
+        query(getAlbum(fakeAlbums[1].id)).then(res =>
+          expect(res.data).toEqual({
+            album: {
+              userId: fakeAlbums[1].userId,
+              id: fakeAlbums[1].id,
+              title: fakeAlbums[1].title,
+              photos: fakePhotos
+            }
+          })
+        );
+        query(getAlbum(fakeAlbums[2].id)).then(res =>
+          expect(res.data).toEqual({
+            album: {
+              userId: fakeAlbums[2].userId,
+              id: fakeAlbums[2].id,
+              title: fakeAlbums[2].title,
+              photos: fakePhotos
+            }
+          })
+        );
+      });
+    });
+    it('should get all albums properly', () => {
+      const fakeAlbumsProm = albumFactory.attributesMany(5).then(fakeAlbumsToMock => {
+        axios.setMockAlbums(fakeAlbumsToMock);
+        return fakeAlbumsToMock;
+      });
+      const fakePhotosProm = photoFactory.attributesMany(5).then(fakePhotosToMock => {
+        axios.setMockPhotos(fakePhotosToMock);
+        return fakePhotosToMock;
+      });
+      Promise.all([fakeAlbumsProm, fakePhotosProm]).then(([fakeAlbums, fakePhotos]) => {
+        query(getAlbums()).then(res => expect(res.data.albums).toHaveLength(5));
+        query(getAlbumsWithOffset(0, 3)).then(res => expect(res.data.albums).toHaveLength(3));
+        query(getAlbumsWithFilter(fakeAlbums[2].title)).then(res => {
+          expect(res.data.albums).toHaveLength(1);
+          expect(res.data.albums[0]).toEqual({
             id: fakeAlbums[2].id,
             title: fakeAlbums[2].title,
             photos: fakePhotos
-          }
-        })
-      );
-    });
-    it('should get all albums properly', async () => {
-      const fakeAlbums = await albumFactory.attributesMany(5);
-      const fakePhotos = await photoFactory.attributesMany(5);
-      await axios.setMockAlbums(fakeAlbums);
-      await axios.setMockPhotos(fakePhotos);
-      await query(getAlbums()).then(res => expect(res.data.albums).toHaveLength(5));
-      await query(getAlbumsWithOffset(0, 3)).then(res => expect(res.data.albums).toHaveLength(3));
-      await query(getAlbumsWithFilter(fakeAlbums[2].title)).then(res => {
-        expect(res.data.albums).toHaveLength(1);
-        expect(res.data.albums[0]).toEqual({
-          id: fakeAlbums[2].id,
-          title: fakeAlbums[2].title,
-          photos: fakePhotos
+          });
         });
       });
     });
