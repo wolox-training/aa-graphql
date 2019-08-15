74120620aae898479d0d317ed648987aa36e9c8c
aleacevedo <ale.acevedo@live.com.ar>
2019-08-15 17:48:05 -0300

    Add caching to album service

--- app/services/album.js
+++ app/services/album.js
@@ -3,27 +3,76 @@ const { url } = require('../../config').common.api;
 const errors = require('../errors');
 const { user: User } = require('../models');
 const { transaction: Transaction } = require('../models');
+const redisClient = require('redis').createClient();
+const DataLoader = require('dataloader');
+const RedisDataLoader = require('redis-dataloader')({ redis: redisClient });
 
-exports.getAlbum = id => {
-  const endpoint = `${url}albums/${id}`;
-  return axios
-    .get(endpoint)
-    .then(response => response.data)
-    .catch(e => {
-      throw errors.conectionError(e.message);
-    });
-};
+const albumLoader = new RedisDataLoader(
+  'album',
+  new DataLoader(
+    arrayId =>
+      Promise.resolve(
+        arrayId.map(id => {
+          const endpoint = `${url}albums/${id}`;
+          return axios.get(endpoint);
+        })
+      ),
+    { cache: false }
+  ),
+  {
+    // caching here is a local in memory cache. Caching is always done
+    // to redis.
+    cache: true,
+    serialize: info => JSON.stringify(info.data)
+  }
+);
 
-exports.getPhotosOfAlbum = albumId => {
-  const query = `?albumId=${albumId}`;
-  const endpoint = `${url}photos${query}`;
-  return axios
-    .get(endpoint)
-    .then(response => response.data)
-    .catch(e => {
-      throw errors.conectionError(e.message);
-    });
-};
+const allAlbumsLoader = new RedisDataLoader(
+  'allAlbums',
+  new DataLoader(arrayId =>
+    Promise.resolve(
+      arrayId.map(() => {
+        const endpoint = `${url}albums`;
+        return axios.get(endpoint);
+      })
+    )
+  ),
+  {
+    // caching here is a local in memory cache. Caching is always done
+    // to redis.
+    cache: true,
+    serialize: info => JSON.stringify(info.data)
+  }
+);
+
+const photosLoader = new RedisDataLoader(
+  'photos',
+  new DataLoader(arrayAlbumId =>
+    Promise.resolve(
+      arrayAlbumId.map(albumId => {
+        const query = `?albumId=${albumId}`;
+        const endpoint = `${url}photos${query}`;
+        return axios.get(endpoint);
+      })
+    )
+  ),
+  {
+    // caching here is a local in memory cache. Caching is always done
+    // to redis.
+    cache: true,
+    serialize: info => JSON.stringify(info.data)
+  }
+);
+
+exports.getAlbum = id =>
+  albumLoader.load(id).catch(e => {
+    throw errors.conectionError(e.message);
+  });
+
+exports.getPhotosOfAlbum = albumId =>
+  photosLoader.load(albumId).catch(e => {
+    throw errors.conectionError(e.message);
+  });
 
 const sortFunction = (array, orderBy) =>
   array.sort((a, b) => {
@@ -39,11 +88,9 @@ const sortFunction = (array, orderBy) =>
     return 0;
   });
 
-exports.getAllAlbums = (offset, limit, filter, orderBy) => {
-  const endpoint = `${url}albums`;
-  return axios
-    .get(endpoint)
-    .then(response => response.data)
+exports.getAllAlbums = (offset, limit, filter, orderBy) =>
+  allAlbumsLoader
+    .load('NaN')
     .catch(e => {
       throw errors.conectionError(e.message);
     })
@@ -57,7 +104,6 @@ exports.getAllAlbums = (offset, limit, filter, orderBy) => {
       }
       return processedAlbums.slice(offset, limit);
     });
-};
 
 exports.buyAlbum = async (albumId, context) => {
   const { user } = context;
