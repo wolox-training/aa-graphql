819441761c246001ec55e4d38d54ec30880fb0b9
aleacevedo <ale.acevedo@live.com.ar>
2019-08-22 10:33:07 -0300

    Add files necesary for autoimports

--- app/graphql/types.js
+++ app/graphql/types.js
@@ -1,27 +1,6 @@
 const { gql } = require('apollo-server');
 
 module.exports = gql`
-  enum CacheControlScope {
-    PUBLIC
-    PRIVATE
-  }
-  directive @cacheControl(maxAge: Int, scope: CacheControlScope) on FIELD_DEFINITION | OBJECT | INTERFACE
-  type Query
-  type Mutation
-  type Subscription
-  type User @cacheControl(maxAge: 240) {
-    firstName: String! @deprecated(reason: "We join in name")
-    lastName: String! @deprecated(reason: "We join in name")
-    name: String!
-    email: String!
-    password: String!
-    id: ID!
-  }
-  type AccessToken {
-    accessToken: String!
-    #refreshToken: String!
-    #expiresIn: Int!
-  }
   type Album @cacheControl(maxAge: 240) {
     id: ID!
     title: String!
