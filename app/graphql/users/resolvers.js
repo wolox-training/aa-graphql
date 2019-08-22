978ad59c21761960a2abc952320ad12a21eff4df
aleacevedo <ale.acevedo@live.com.ar>
2019-08-22 10:43:22 -0300

    Refactor resolver

--- app/graphql/users/resolvers.js
+++ app/graphql/users/resolvers.js
@@ -1,20 +1,14 @@
 const { getAll: getAllUsers, getOne: getOneUser } = require('../../models').user,
   { createUser, logIn } = require('../../services/user');
 
-const getUser = (_, params) => getOneUser(params);
-const getUsers = (_, params) => getAllUsers(params);
-
-const newUser = (_, { user }) => createUser(user);
-const userLogIn = (_, { logInInput }) => logIn(logInInput);
-
 module.exports = {
   Query: {
-    user: getUser,
-    users: getUsers
+    user: (_, params) => getOneUser(params),
+    users: (_, params) => getAllUsers(params)
   },
   Mutation: {
-    user: newUser,
-    logIn: userLogIn
+    user: (_, { user }) => createUser(user),
+    logIn: (_, { logInInput }) => logIn(logInInput)
   },
   User: {
     name: obj => `${obj.firstName} ${obj.lastName}`
